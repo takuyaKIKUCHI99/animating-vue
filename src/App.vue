15cd2cb487b274866d4e2b2a4f58b908eac1474c
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-08-23 11:26:15 +0900

    page transitions, slide-fade

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <transition name="fade" mode="out-in">
+    <transition name="slide-fade" mode="out-in">
       <router-view />
     </transition>
   </div>
@@ -66,6 +66,21 @@ button {
 }
 
 .fade-leave-to {
+  opacity: 0;
+}
+
+.slide-fade-enter {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all .2s ease;
+}
+
+.slide-fade-leave-to {
+  transform: translateX(-10px);
   opacity: 0;
 }
 </style>
