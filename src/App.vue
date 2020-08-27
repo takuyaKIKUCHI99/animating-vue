079d26cf47cef0bd9b63239dc16e5bfa3bf66201
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-08-27 09:33:51 +0900

    JavaScript Hooks + Velocity

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/list">List</router-link>
+      <router-link to="/list">List</router-link> |
+      <router-link to="/drawer">Drawer</router-link>
     </div>
     <transition name="slide-fade" mode="out-in">
       <router-view />
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Drawer from "./views/Drawer.vue";
 
 Vue.use(Router);
 
@@ -21,6 +22,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/List.vue")
-    }
+    },
+    {
+      path: "/drawer",
+      name: "drawer",
+      component: Drawer
+    },
   ]
 });
