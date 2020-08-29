601a009c3dc402c6a5d625689fd0353a65fec8fc
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-08-29 11:12:53 +0900

    Timelines with GSAP

--- src/router.js
+++ src/router.js
@@ -52,11 +52,11 @@ export default new Router({
       component: () =>
         import(/* webpackChunkName: "state" */ './views/State.vue')
     },
-    // {
-    //   path: '/timeline',
-    //   name: 'timeline',
-    //   component: () =>
-    //     import(/* webpackChunkName: "timeline" */ './views/Timeline.vue')
-    // }
+    {
+      path: '/timeline',
+      name: 'timeline',
+      component: () =>
+        import(/* webpackChunkName: "timeline" */ './views/Timeline.vue')
+    }
   ]
 });
